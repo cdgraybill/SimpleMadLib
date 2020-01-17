2b16d5a01541f9fd8363bdb6a38884963e182fe6
Cameron Graybill <cdgraybill@gmail.com>
2020-01-17 12:51:05 -0600

    added some changes

---
+++ MadLibExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MadLibExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Intro();
+            Console.WriteLine($"{GetName()} decided to walk his {getNoun()} to the {getPlace()}.");
+            Console.ReadLine();
+        }
+
+        public static void Intro()
+        {
+            Console.WriteLine("Let's do a MadLib");
+            Console.ReadLine();
+        }
+
+        public static string GetName()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        public static string getNoun()
+        {
+            Console.WriteLine("State a noun");
+            string noun = Console.ReadLine();
+            return noun;
+        }
+
+        public static string getPlace()
+        {
+            Console.WriteLine("State a place");
+            string place = Console.ReadLine();
+            return place;
+        }
+
+    }
+}
